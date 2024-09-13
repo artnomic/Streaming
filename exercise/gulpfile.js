5620c9686e8dca87d5f42e58e0e71c3c89ed4914
artnomic <arthurpe44@gmail.com>
2024-09-13 11:46:40 -0300

    feat: finish exercise

--- exercise/gulpfile.js
+++ exercise/gulpfile.js
@@ -1,13 +1,5 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify');
-
-function scripts() {
-    return gulp.src('./src/scripts/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'));
-}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -15,16 +7,8 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-function images() {
-    return gulp.src('./src/images/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
-}
-
-
-exports.default = gulp.parallel(styles, images, scripts);
+exports.default = gulp.parallel(styles);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
